10008fe0a69ff928cebe770e7d383d56f50dc150
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-06-02 15:06:25 +0530

    Add files via upload

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] dateParts = input.Split(' ');
+
+        int month = int.Parse(dateParts[0]);
+        int day = int.Parse(dateParts[1]);
+        int year = int.Parse(dateParts[2]);
+
+        DateTime date = new DateTime(year, month, day);
+        DayOfWeek dayOfWeek = date.DayOfWeek;
+
+        string dayOfWeekString = dayOfWeek.ToString().ToUpper();
+        Console.WriteLine(dayOfWeekString);
+    }
+}
---
+++ Dequeue/Dequeue/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] nm = Console.ReadLine().Split(' ');
+        int n = int.Parse(nm[0]);
+        int m = int.Parse(nm[1]);
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+        int maxUnique = FindMaxUniqueIntegers(arr, m);
+        Console.WriteLine(maxUnique);
+    }
+
+    static int FindMaxUniqueIntegers(int[] arr, int m)
+    {
+        int maxUnique = 0;
+        Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+
+        for (int i = 0; i < m; i++)
+        {
+            if (frequencyMap.ContainsKey(arr[i]))
+                frequencyMap[arr[i]]++;
+            else
+                frequencyMap[arr[i]] = 1;
+        }
+
+        maxUnique = frequencyMap.Count;
+
+        for (int i = m; i < arr.Length; i++)
+        {
+            if (frequencyMap[arr[i - m]] == 1)
+                frequencyMap.Remove(arr[i - m]);
+            else
+                frequencyMap[arr[i - m]]--;
+
+            if (frequencyMap.ContainsKey(arr[i]))
+                frequencyMap[arr[i]]++;
+            else
+                frequencyMap[arr[i]] = 1;
+
+            maxUnique = Math.Max(maxUnique, frequencyMap.Count);
+        }
+
+        return maxUnique;
+    }
+}
